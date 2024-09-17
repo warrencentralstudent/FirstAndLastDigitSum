7e22fd3876fab442a2ed13db0366335216539f4b
Davian Affognon <affogdav000@warren.k12.in.us>
2024-09-17 11:21:54 -0400

    man

--- src/FirstAndLastDigitSum.java
+++ src/FirstAndLastDigitSum.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class FirstAndLastDigitSum {
+class Main {
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(257));
+        System.out.println(sumFirstAndLastDigit(0));
+        System.out.println(sumFirstAndLastDigit(5));
+        System.out.println(sumFirstAndLastDigit(-10));
+    }
+
+    private static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int lastDigit = number % 10;
+        //Get the last digit:  mod the number by 10:
+        // store in some variable
+
+        int firstDigit = 0;
+
+        while (number > 0){
+            firstDigit = number;
+            number /= 10;
+        }
+
+        // While number > 0
+        // set firstDigit to number
+        // divide equals number by 10
+
+        return firstDigit + lastDigit;
+    }
 }
